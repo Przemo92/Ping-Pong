1de5d991ba17b6030336b644b4a598f31dc2f481
Przemek Kapela <przmek.kapela92@gmail.com>
2020-02-23 20:58:33 +0100

    Add movements for paddles and ball

--- Unit1.h
+++ Unit1.h
@@ -7,10 +7,30 @@
 #include <Controls.hpp>
 #include <StdCtrls.hpp>
 #include <Forms.hpp>
+#include <ExtCtrls.hpp>
+#include <Graphics.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
+        TShape *backGround;
+        TImage *ball;
+        TTimer *timerBall;
+        TImage *paddle1;
+        TImage *paddle2;
+        TTimer *turnUp1;
+        TTimer *turnDown1;
+        TTimer *turnUp2;
+        TTimer *turnDown2;
+        void __fastcall timerBallTimer(TObject *Sender);
+        void __fastcall turnUp1Timer(TObject *Sender);
+        void __fastcall turnDown1Timer(TObject *Sender);
+        void __fastcall FormKeyUp(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall FormKeyDown(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall turnUp2Timer(TObject *Sender);
+        void __fastcall turnDown2Timer(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
         __fastcall TForm1(TComponent* Owner);
